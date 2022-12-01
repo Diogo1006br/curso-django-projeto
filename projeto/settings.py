fd5d1143b6b919401e15b527efec6301669ea99a
Diogo1006br <dramuski@gmail.com>
2022-12-01 10:41:07 -0300

    Modificado a parte do html , e configrando static

--- projeto/settings.py
+++ projeto/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # a gente tem apps tambem
     'recipes',
 
 ]
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STACTICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
